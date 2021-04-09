882fc606ad67715f6735aea63737d16ec8eee80e
Christian Aurich <christian.aurichzm@gmail.com>
2021-04-09 04:59:41 -0300

    Improve ImageFilterController indentation

--- br/com/christian/imagefilters/controller/filter/ImageFilterController.java
+++ br/com/christian/imagefilters/controller/filter/ImageFilterController.java
@@ -7,20 +7,20 @@ import br.com.christian.imagefilters.controller.storage.ImageStorage;
 public class ImageFilterController {
     private final ImageStorage imageStorage = ImageStorage.getInstance();
 
-	private static ImageFilterController imageFilterController;
-    
+    private static ImageFilterController imageFilterController;
+
     public static ImageFilterController getInstance() {
-    	if (imageFilterController == null) {
-    		imageFilterController = new ImageFilterController();
-    	}
-    	return imageFilterController;
+        if (imageFilterController == null) {
+            imageFilterController = new ImageFilterController();
+        }
+        return imageFilterController;
     }
-	
-	public void applyFilter(ImageFilter imageFilter, String filename) {
+
+    public void applyFilter(ImageFilter imageFilter, String filename) {
         BufferedImage image = imageStorage.loadImage(filename);
-		int width = image.getWidth();
+        int width = image.getWidth();
         int height = image.getHeight();
-  
+
         for (int y = 0; y < height; y++) {
             for (int x = 0; x < width; x++) {
                 int p = image.getRGB(x, y);
@@ -28,6 +28,6 @@ public class ImageFilterController {
             }
         }
         imageStorage.saveImage(image);
-	}
+    }
 
 }

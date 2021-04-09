f213acb1b21c68f78714f77ce2ac42484a10cbab
Christian Aurich <christian.aurichzm@gmail.com>
2021-04-09 04:35:32 -0300

    Add ImageFilter, ImageFilterController

---
+++ br/com/christian/imagefilters/controller/filter/ImageFilter.java
@@ -0,0 +1,5 @@
+package br.com.christian.imagefilters.controller.filter;
+
+public interface ImageFilter {
+	public int filterARGB(int x, int y, int rgb);
+}
---
+++ br/com/christian/imagefilters/controller/filter/ImageFilterController.java
@@ -0,0 +1,33 @@
+package br.com.christian.imagefilters.controller.filter;
+
+import java.awt.image.BufferedImage;
+
+import br.com.christian.imagefilters.controller.storage.ImageStorage;
+
+public class ImageFilterController {
+	private final ImageStorage imageStorage = ImageStorage.getInstance();
+
+	private static ImageFilterController imageFilterController;
+    
+    public static ImageFilterController getInstance() {
+    	if (imageFilterController == null) {
+    		imageFilterController = new ImageFilterController();
+    	}
+    	return imageFilterController;
+    }
+	
+	public void applyFilter(ImageFilter imageFilter, String filename) {
+        BufferedImage image = imageStorage.loadImage(filename);
+		int width = image.getWidth();
+        int height = image.getHeight();
+  
+        for (int y = 0; y < height; y++) {
+            for (int x = 0; x < width; x++) {
+                int p = image.getRGB(x, y);
+                image.setRGB(x, y, imageFilter.filterARGB(x, y, p));
+            }
+        }
+        imageStorage.saveImage(image);
+	}
+
+}
